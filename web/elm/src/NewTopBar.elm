9b2aefef42a293c49d9a92d2f0d9aee56ff9e36f
Jamie Klassen <cklassen@pivotal.io>
2018-10-17 21:36:12 -0400

    giving up on '/' test
    turns out it's a bit tricky to test subscriptions/html events
    in Elm. maybe worth using
    https://package.elm-lang.org/packages/rogeriochaves/elm-testable/latest/
    at some point.
    
    This reverts commit 00fcfdd8957582fe00a827bacbc72f5eb132c857.

--- web/elm/src/NewTopBar.elm
+++ web/elm/src/NewTopBar.elm
@@ -228,8 +228,8 @@ update msg model =
                 ( newModel, Cmd.none )
 
         KeyDown keycode ->
-            case ( model.searchBar, keycode ) of
-                ( Expanded r, _ ) ->
+            case model.searchBar of
+                Expanded r ->
                     if not r.showAutocomplete then
                         ( { model | searchBar = Expanded { r | selectionMade = False, selection = 0 } }, Cmd.none )
                     else
@@ -273,21 +273,6 @@ update msg model =
                             _ ->
                                 ( { model | searchBar = Expanded { r | selectionMade = False, selection = 0 } }, Cmd.none )
 
-                -- '/' key
-                ( Collapsed, 191 ) ->
-                    ( { model
-                        | searchBar =
-                            Expanded
-                                { query = ""
-                                , selectionMade = False
-                                , showAutocomplete = False
-                                , selection = 0
-                                , screenSize = Mobile
-                                }
-                      }
-                    , Task.attempt (always Noop) (Dom.focus "search-input-field")
-                    )
-
                 _ ->
                     ( model, Cmd.none )
 
@@ -295,21 +280,39 @@ update msg model =
             showSearchInput model
 
         ScreenResized size ->
-            case ( model.searchBar, getScreenSize size ) of
-                ( Expanded r, Mobile ) ->
-                    if String.isEmpty r.query then
-                        ( { model | searchBar = Collapsed }, Cmd.none )
-                    else
-                        ( { model | searchBar = Expanded { r | screenSize = Mobile } }, Cmd.none )
+            let
+                newSize =
+                    getScreenSize size
 
-                ( Expanded r, Desktop ) ->
-                    ( { model | searchBar = Expanded { r | screenSize = Desktop } }, Cmd.none )
+                newModel =
+                    case ( model.searchBar, newSize ) of
+                        ( Expanded r, _ ) ->
+                            case ( r.screenSize, newSize ) of
+                                ( Desktop, Mobile ) ->
+                                    if String.isEmpty r.query then
+                                        { model | searchBar = Collapsed }
+                                    else
+                                        { model | searchBar = Expanded { r | screenSize = newSize } }
 
-                ( Collapsed, Desktop ) ->
-                    showSearchInput model
+                                _ ->
+                                    { model | searchBar = Expanded { r | screenSize = newSize } }
 
-                _ ->
-                    ( model, Cmd.none )
+                        ( Collapsed, Desktop ) ->
+                            { model
+                                | searchBar =
+                                    Expanded
+                                        { query = ""
+                                        , selectionMade = False
+                                        , showAutocomplete = False
+                                        , selection = 0
+                                        , screenSize = Desktop
+                                        }
+                            }
+
+                        _ ->
+                            model
+            in
+                ( newModel, Cmd.none )
 
 
 showSearchInput : Model -> ( Model, Cmd Msg )
--- web/elm/tests/DashboardTests.elm
+++ web/elm/tests/DashboardTests.elm
@@ -8,49 +8,12 @@ import RemoteData
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (attribute, class, tag)
-import Test.Html.Event as Event
-import Json.Encode as Encode exposing (Value)
-import NewTopBar
-
-
-updateModel : Msg -> Model -> Model
-updateModel msg =
-    Dashboard.update msg >> Tuple.first
-
-
-queryView : Model -> Query.Single Msg
-queryView =
-    view
-        >> HS.toUnstyled
-        >> Query.fromHtml
 
 
 all : Test
 all =
     describe "Dashboard"
-        [ test "sends a ShowSearchInput message when the '/' button is pressed" <|
-            let
-                simulatedEventObject : Value
-                simulatedEventObject =
-                    Encode.object
-                        [ ( "key", Encode.string "/" )
-                        , ( "code", Encode.string "Slash" )
-                        ]
-            in
-                \_ ->
-                    init
-                        { title = always Cmd.none
-                        }
-                        { csrfToken = ""
-                        , turbulencePath = ""
-                        , search = ""
-                        , highDensity = False
-                        }
-                        |> Tuple.first
-                        |> queryView
-                        |> Event.simulate (Event.custom "keypress" simulatedEventObject)
-                        |> Event.expect (TopBarMsg NewTopBar.ShowSearchInput)
-        , test "links to specific builds" <|
+        [ test "links to specific builds" <|
             \_ ->
                 let
                     msg =
@@ -100,30 +63,30 @@ all =
                                   )
                                 )
                 in
-                    init
-                        { title = always Cmd.none
-                        }
-                        { csrfToken = ""
-                        , turbulencePath = ""
-                        , search = ""
-                        , highDensity = False
-                        }
-                        |> Tuple.first
-                        |> update msg
-                        |> Tuple.first
-                        |> view
-                        |> HS.toUnstyled
-                        |> Query.fromHtml
-                        |> Query.find
-                            [ class "dashboard-team-group"
-                            , attribute <| Attr.attribute "data-team-name" "team"
-                            ]
-                        |> Query.find
-                            [ class "node"
-                            , attribute <| Attr.attribute "data-tooltip" "job"
-                            ]
-                        |> Query.find
-                            [ tag "a" ]
-                        |> Query.has
-                            [ attribute <| Attr.href "/teams/team/pipelines/pipeline/jobs/job/builds/1" ]
+                Dashboard.init
+                    { title = always Cmd.none
+                    }
+                    { csrfToken = ""
+                    , turbulencePath = ""
+                    , search = ""
+                    , highDensity = False
+                    }
+                    |> Tuple.first
+                    |> update msg
+                    |> Tuple.first
+                    |> view
+                    |> HS.toUnstyled
+                    |> Query.fromHtml
+                    |> Query.find
+                        [ class "dashboard-team-group"
+                        , attribute <| Attr.attribute "data-team-name" "team"
+                        ]
+                    |> Query.find
+                        [ class "node"
+                        , attribute <| Attr.attribute "data-tooltip" "job"
+                        ]
+                    |> Query.find
+                        [ tag "a" ]
+                    |> Query.has
+                        [ attribute <| Attr.href "/teams/team/pipelines/pipeline/jobs/job/builds/1" ]
         ]
